b50f901e1a3e36727bffb767b09be51291fd1cee
PJ Passalacqua <pjpassa@gmail.com>
2015-06-30 09:01:13 -0400

    got one page working

--- movieratings/ratings/views.py
+++ movieratings/ratings/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from ratings.models import Links
 
 # Create your views here.
+def ratings_links(request):
+    links = Links.objects.all()
+    context = {"links": links}
+    return render_to_response("links.html", context)
