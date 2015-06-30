7842b1903f403c72526384667591e07e6c3be152
PJ Passalacqua <pjpassa@gmail.com>
2015-06-30 11:55:23 -0400

    actually added initial data load script

--- Scripts/initial_data_load.py
+++ Scripts/initial_data_load.py
@@ -1,1 +1,29 @@
-__author__ = 'pjpassalacqua'
+from ratings.models import Rater, Movie, Link, Tag, Rating
+
+raters = set()
+with open("../ml-20m/ratings.csv") as file:
+    lines = file.readlines()
+
+for num, line in enumerate(lines):
+    if num == 0:
+        continue
+    data = line.split(",")
+    raters.add(data[0])
+    #Rating(user=data[0], movie=data[1], rating=data[2],timestame=data[3]).save()
+
+
+with open("../ml-20m/tags.csv") as file:
+    lines = file.readlines()
+
+for num, line in enumerate(lines):
+    if num == 0:
+        continue
+    data = line.split(",")
+    raters.add(data[0])
+    #Rating(user=data[0], movie=data[1], tag=data[2],timestame=data[3]).save()
+
+raters = sorted(list(raters))
+
+for rater in raters:
+    #Rater(user=rater).save()
+    pass
